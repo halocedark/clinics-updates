9737114cc69070ccd64923b911c51d5753ff545f
abdennour <homer.rider069@gmail.com>
2022-06-22 01:13:42 -0700

    delete

--- PatientPanel/assets/js/include/Classes.js
+++
@@ -1,9 +0,0 @@
-var TheDate = require(__dirname+'/assets/js/utils/date');
-const IniFile = require(__dirname+'/assets/js/utils/IniFile.js');
-var DBH = require(__dirname+'/assets/js/include/DBH');
-
-$(function()
-{
-
-
-});
